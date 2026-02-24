400459b4e56dda09a4c70d03847ae78621d86bf7
Digital_Tokyo <bndrbots@gmail.com>
2026-02-24 15:28:54 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import React, { useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { LazyMotion, domAnimation, m, AnimatePresence, useReducedMotion } from "framer-motion";
 import { ArrowUpRight, ExternalLink, Flame, X } from "lucide-react";
