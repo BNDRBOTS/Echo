2c5d11a11129f4710db9ec8e296ed871005a7739
Digital_Tokyo <bndrbots@gmail.com>
2026-02-25 18:48:37 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SpotlightLayout from "./components/SpotlightLayout";
-import ScenePage from "./pages/ScenePage";
-import GraffitiWall from "./pages/GraffitiWall";
-import Directives from "./pages/Directives";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-export function App() {
-  return (
-    <BrowserRouter>
-      <SpotlightLayout>
-        <Routes>
-          <Route path="/" element={<ScenePage />} />
-          <Route path="/yard" element={<GraffitiWall />} />
-          <Route path="/directives" element={<Directives />} />
-        </Routes>
-      </SpotlightLayout>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
